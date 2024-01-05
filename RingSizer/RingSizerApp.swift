fc8e3906c917a2e7f3b1661be5a94a78cc9d4369
Cisino Junior <cisino.gomes@interatedev.com.br>
2024-01-05 16:39:35 -0300

    Initial Commit

---
+++ RingSizer/RingSizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RingSizerApp.swift
+//  RingSizer
+//
+//  Created by Cisino Junior on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RingSizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
